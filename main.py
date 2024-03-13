a4383312a38dc9fb46cd1645a6efba1bc9429876
oh-hyunjin <hyunjin6918@naver.com>
2024-03-13 19:48:06 +0900

    오현진 2021112482

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 print("--- input ---")
 name = input("name: ")
 student_id = input("student id: ")
+major = input("major: ")
 
 print("--- output ---")
 print("name: ", name)
 print("student id: ", student_id)
+print("major: ", major)
